331b0e55afc5774e7a77f61ef9d06a754a00d5e1
JonR45 <jonathonreid45@gmail.com>
2023-11-22 22:16:28 +0000

    defined split_into_video_series function
    created split_into_video_series function and renamed files for clearer organisation

--- src/exploration/data_manipulation.py
+++ src/exploration/data_manipulation.py
@@ -7,6 +7,10 @@ Functions include:
     `single_metric_dataframes(df, metrics=[]`:
         Takes a list of metrics and creates individual dataframes sorted by the given
         metric in descnding order
+    
+    `split_into_video_series(df, video_series_title=[])`:
+        Takes a string, or list of strings, and returns a dataframe with only those strings
+        in the video title.
 
     
 
@@ -14,8 +18,6 @@ Functions include:
 
 
 import pandas as pd
-
-
 
 
 def single_metric_dataframes(df, metrics=[]):
@@ -44,12 +46,21 @@ def single_metric_dataframes(df, metrics=[]):
     Single metric error:
         Added logic that checked the length of the list generated during the execution of the function.
         This was because when there was only a single metric provided, a list was being returned insead of 
-        the dataframe contained in the list. Explicitly transforming the single item to a dataframe and 
-        then returning the dataframe resolved this error.
+        the dataframe contained in the list. Explicitly transforming the single item to a dataframe with line
+        
+        df_sorted = pd.DataFrame(df_list[0])
+        
+        and  then returning the dataframe resolved this error.
     
     """
+        
+    # check if input is a single string, if it is then store it as a single item list
+    if isinstance(metrics, str):
+        metrics = [metrics]
+        
+    elif isinstance(metrics, list):
+        metrics = [x for x in metrics]
     
-    metrics = list(metrics)
     
     df_list = []
     
@@ -59,7 +70,7 @@ def single_metric_dataframes(df, metrics=[]):
         v = str(v)
         
         try: 
-            df_ = (df[['videoID', 'title', v]]
+            df_ = (df[['videoID', 'title', 'publishedAt', v]]
                 .sort_values(by=[v], ascending=False, axis=0)
                 .reset_index(drop=True))
 
@@ -89,6 +100,82 @@ def single_metric_dataframes(df, metrics=[]):
         
         return [df for df in df_list]
 
-    
 
+
+
+def split_into_video_series(df, video_series_title=[]):
+    """Takes in a dataframe and the name of a YouTube video series and returns the input dataframe with only
+    the videos whose titles include the 'video_series_title' string in their title.
+    
+    Parameters
+    ----------
+    df : pandas dataframe
+        Dataframe with YouTube video data.
+        
+    video_series_title : list
+        List of strings of the title, or part of the title, of the video series we want to return a dataframe for.
+        
+    Returns
+    -------
+    df_ : dataframe
+        Dataframe with only the rows whose video titles contain the specified 'video_series_title'.
+        
+    
+    Notes
+    ------
+    
+    
+    """
+    
+    # check if input is a single string, if it is then store it as a single item list
+    if isinstance(video_series_title, str):
+        video_series_title = [video_series_title]
+        
+    # if input is a list then transform to lower case 
+    elif isinstance(video_series_title, list):
+        video_series_title = [x.lower() for x in video_series_title]
+    
+    
+    # create empty list that will store dataframes which will be concatenated later
+    df_list = []
+    
+    # check length of video_series_title list; if greater than 1 create multiple dataframes and then concatenate
+    if len(video_series_title) > 1: 
+        # loop through strings of video_series_title list
+        for i, v in enumerate(video_series_title):
+            
+            # create dataframe with only the video titles that contain the specified string and append to list
+            df_ = df.loc[df['title'].str.contains(v, case=False)] 
+            df_list.append(df_)
+            
+        # concatenate dataframes, perform sorting and cleaning operations
+        df_combined = pd.concat(df_list, ignore_index=True)
+        df_combined = (df_combined
+                       .sort_values(by=['publishedAt'])
+                       .drop_duplicates()
+                       .reset_index(drop=True))
+        
+        # return the dataframe
+        return df_combined
+        
+            
+    # check length of video_series_title list; if equal to 1 then use only item in list to create the dataframe
+    elif len(video_series_title) == 1: 
+        # create dataframe with only the video titles that contain the specified string and append to list
+        df_ = df.loc[df['title'].str.contains(video_series_title[0], case=False)]
+        # perform sorting and cleaning operations
+        df_ = (df_
+               .sort_values(by=['publishedAt'])
+               .drop_duplicates()
+               .reset_index(drop=True))
+           
+        # return dataframe
+        return df_
+    
+    else: # no video series names/titles provided
+        print("No changes made.")
+        
+        return df
+
+    
     
